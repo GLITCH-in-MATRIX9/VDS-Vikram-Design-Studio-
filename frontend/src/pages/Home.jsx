205beb9bc9f77983f9a339769815f09d07af38b5
CoderKarsh <shreeut2704@gmail.com>
2025-09-20 21:16:22 +0530

    Add clamp to animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
   }
 
   // Mapping scrollVelocity to z perspective
-  const targetZ = useTransform(scrollVelocity, [-1500, 0, 1500], [-150, 0, -150], { clamp: false });
+  const targetZ = useTransform(scrollVelocity, [-1500, 0, 1500], [-150, 0, -150], { clamp: true });
   const z = useSpring(targetZ, springConfig);
   
   // Keep the origin of perspective change in the center of the screen dynamically
