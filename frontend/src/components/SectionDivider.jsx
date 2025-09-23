c7bc543c3ba79ddca62d0e83e5ea2a3187b9f207
CoderKarsh <shreeut2704@gmail.com>
2025-09-24 01:55:04 +0530

    Change shadow colour of the SectionDivider

--- frontend/src/components/SectionDivider.jsx
+++ frontend/src/components/SectionDivider.jsx
@@ -1,7 +1,7 @@
 const SectionDivider = () => {
     return (
         <div className="section-divider w-screen">
-            <hr className="w-screen h-0.5 text-[#EAE2DC] shadow-[0_1.5px_0_\#F8F4F1\]"/>
+            <hr className="w-screen h-0.5 text-[#EAE2DC] shadow-[0_1.5px_0_rgb(248,247,248)]"/>
         </div>
     )
 }
