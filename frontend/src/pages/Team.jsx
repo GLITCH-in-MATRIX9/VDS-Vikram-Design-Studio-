341d4cf183b3e9256afc7f62307792b5062e6d77
CoderKarsh <shreeut2704@gmail.com>
2025-10-15 21:13:31 +0530

    Add Section divider

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Heading from "../components/Teams/Heading";
 import MarqueeImages from "../components/Teams/MarqueeImages";
 import TeamMembers from "../components/Teams/TeamMembers";
+import SectionDivider from "../components/SectionDivider";
 
 const Team = () => {
   return (
     <div className="bg-[#F2EFEE]">
       <Heading />
       <MarqueeImages />
+      <SectionDivider />
       <TeamMembers />
     </div>
   );
